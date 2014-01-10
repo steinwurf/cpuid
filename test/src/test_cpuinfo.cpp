9e3765c32bc2fc8e360780c365b81423f69cfb9e
Nestor J Hernandez M <nestor@steinwurf.com>
2014-01-10 13:30:04 +0100

    minor change

--- test/src/test_cpuinfo.cpp
+++ test/src/test_cpuinfo.cpp
@@ -19,5 +19,4 @@ TEST(cpuinfo_tests, check_instruction_sets)
               m_cpuinfo.has_pclmulqdq());
     EXPECT_EQ(variable_map["has_avx"].as<bool>(), m_cpuinfo.has_avx());
     EXPECT_EQ(variable_map["has_neon"].as<bool>(), m_cpuinfo.has_neon());
-
 }
