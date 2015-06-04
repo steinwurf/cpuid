9d188b97cb6a9f6f4dadb9cc3493d47c4636a89c
Peter Vingelmann <petya@master.com>
2015-06-04 02:05:28 +0200

    Add buildbot.py

---
+++ buildbot.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+# encoding: utf-8
+
+project_name = 'cpuid'
+
+def configure(options):
+    pass
+
+def build(options):
+    pass
+
+def run_tests(options):
+    pass
+
+def coverage_settings(options):
+    options['required_line_coverage'] = 50.0
