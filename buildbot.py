e9b68ef4844208b3446b33378a4cc0ed815f8bd0
Jeppe Pihl <jpihl08@gmail.com>
2019-07-24 14:25:39 +0200

    add cmake buildbot support

--- buildbot.py
+++ buildbot.py
@@ -83,6 +83,20 @@ def install(properties):
     run_command(command)
 
 
+def cmake(properties):
+    build_path = 'build'
+    if os.path.exists(build_path):
+        print("Path '{}' already exists - removing".format(build_path))
+        shutil.rmtree(build_path)
+    os.mkdir(build_path)
+
+    old_cwd = os.getcwd()
+    os.chdir(build_path)
+    run_command(['cmake', '../'])
+    run_command(['cmake', '--build', '.'])
+    os.chdir(old_cwd)
+
+
 def coverage_settings(options):
     options['required_line_coverage'] = 100.0
     options['run_cmd'] = '%s --no_fail'
@@ -106,6 +120,8 @@ def main():
         run_tests(properties)
     elif cmd == 'install':
         install(properties)
+    elif cmd == 'cmake':
+        cmake(properties)
     else:
         print("Unknown command: {}".format(cmd))
 
